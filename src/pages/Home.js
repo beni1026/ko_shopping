f091b414c611a8544491879610b036db5155a68f
EUNA <beni1026@naver.com>
2021-12-17 16:59:32 +0900

    refactor link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
+
+
     render() {
+
         return (
             <div>
                 <h3> Home 페이지 입니다. </h3>
-                <a href="Detail"> 디테일페이지 </a>
-                <a href="Basket"> 장바구니페이지</a>
+                <a href="ko_shopping/detail"> 디테일페이지 </a><br/>
+                <a href="ko_shopping/basket"> 장바구니페이지</a>
             </div>
         );
     }
