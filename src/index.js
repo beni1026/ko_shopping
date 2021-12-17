0c627b3c3b37b225002b5dec0304ef241a9c3445
EUNA <beni1026@naver.com>
2021-12-17 14:00:24 +0900

    add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Link} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter >
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
       </BrowserRouter>
   </React.StrictMode>,
